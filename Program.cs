78012b90b5ef127a4a0e3b1e2a52b84f7bb65ce5
Mikhail T <Mikola301@mail.ru>
2023-08-02 17:23:07 +0700

    add fn TransformationArrayWhen3chart

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+//  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+string[] array1 = new string[6] { "water", "sun", "3254", "mir", "tigr", "20" };
+
+string[] array2 = new string[array1.Length];
+
+void TransformationArrayWhen3chart(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
